6e58b15656723af3bf282fe9b92fb74e9c730be4
Sahil Moon <944sahil@gmail.com>
2026-01-21 10:59:47 +0530

    Add router link

--- src/app/past-bookings/past-bookings.ts
+++ src/app/past-bookings/past-bookings.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-past-bookings',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './past-bookings.html',
   styleUrl: './past-bookings.scss',
 })
--- src/app/upcoming-bookings/upcoming-bookings.ts
+++ src/app/upcoming-bookings/upcoming-bookings.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-upcoming-bookings',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './upcoming-bookings.html',
   styleUrl: './upcoming-bookings.scss',
 })
